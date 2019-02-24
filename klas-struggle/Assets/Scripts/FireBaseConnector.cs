6019f521ea1a560468394877d6af2a17b16aae34
Petr Houška <houskape@gmail.com>
2019-02-24 16:39:30 +0100

    Add intial skelton of firebase connector.

---
+++ klas-struggle/Assets/Scripts/FireBaseConnector.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts;
+using SimpleFirebaseUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBaseConnector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Firebase firebase = Firebase.CreateNew("klas-struggle.firebaseapp.com", Secrets.FirebaseAPIKey);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
