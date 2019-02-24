b0b4957d555ab13e49e228aac5d7f94779c3858b
Petr Houška <houskape@gmail.com>
2019-02-24 18:17:25 +0100

    Test of firebase communication.

--- klas-struggle/Assets/Scripts/FireBaseConnector.cs
+++ klas-struggle/Assets/Scripts/FireBaseConnector.cs
@@ -1,5 +1,6 @@
 ﻿using Assets.Scripts;
 using SimpleFirebaseUnity;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,9 +10,18 @@ public class FireBaseConnector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Firebase firebase = Firebase.CreateNew("klas-struggle.firebaseapp.com", Secrets.FirebaseAPIKey);
+        Firebase firebase = Firebase.CreateNew("https://klas-struggle.firebaseio.com", Secrets.FirebaseAPIKey);
+        var database = firebase.Child("asdf", true); // get's a child that can have OnSuccess / OnFail callbacks set
+        //database.SetValue("asdr", false);          // always operates with the whole sub-graph get/set/push
+        database.Push("asdf");
 
+        database.OnGetSuccess += onSuccess;
+        database.GetValue();
+    }
 
+    private void onSuccess(Firebase arg1, DataSnapshot arg2)
+    {
+        Debug.Log(arg2.RawValue);
     }
 
     // Update is called once per frame
