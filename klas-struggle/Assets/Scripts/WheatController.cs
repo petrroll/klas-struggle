76605ee91b60a03493640a9a2500c832286ab34d
Petr Houška <houskape@gmail.com>
2019-02-26 19:44:53 +0100

    Migrated current v. to controller based changes.

--- klas-struggle/Assets/Scripts/WheatController.cs
+++ klas-struggle/Assets/Scripts/WheatController.cs
@@ -7,30 +7,70 @@ using UnityEngine;
 [System.Serializable]
 public class WheatState
 {
+    public int Checkmarks = -1;
+    public int DropDowns = 1;
+
     public float Size = 2.5f;
 }
 
 public class WheatController : MonoBehaviour
 {
+    public List<GameObject> Checkmarks;
+    public List<GameObject> DropDowns;
+
     public WheatState State = new WheatState();
 
-    private void ApplySize()
+    internal void ApplyDecision(Answer answer)
+    {
+        switch (answer.Question.Stage.Id)
+        {
+            case 0:
+                State.Size = answer.Id == 0 ? 5 : 10;
+                ApplySize();
+                break;
+            case 1:
+                State.Checkmarks = answer.Id + answer.Question.Id * 2;
+                ApplyCheckMarks();
+                break;
+            case 2:
+                State.DropDowns = answer.Id;
+                ApplyDropDowns();
+                break;
+            default:
+                Debug.Assert(false, "Unreachable.");
+                break;
+
+        }
+    }
+
+    void SetActiveObject(List<GameObject> objects, int activeObject)
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            objects[i].SetActive(i == activeObject);
+        }
+    }
+
+    void Start()
+    {
+        ApplySize();
+        ApplyCheckMarks();
+        ApplyDropDowns();
+    }
+
+    void ApplySize()
     {
         transform.localScale = new Vector3(State.Size, State.Size);
     }
 
-    internal void ApplyDecision(Answer answer)
+    void ApplyCheckMarks()
     {
-        if(answer.Question.Stage.Id == 0)
-        {
-            State.Size = answer.Id == 0 ? 5 : 10;
-            ApplySize();
-        }
+        SetActiveObject(Checkmarks, State.Checkmarks);
     }
 
-    // Start is called before the first frame update
-    void Start()
+    void ApplyDropDowns()
     {
-        ApplySize();
+        SetActiveObject(DropDowns, State.DropDowns);
     }
+
 }
