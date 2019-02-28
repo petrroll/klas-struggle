f7af4189ed6c76d55568e856a011b44fa095d983
Petr Houška <houskape@gmail.com>
2019-02-28 11:19:14 +0100

    Remove (revoked) APIkey (again).

--- klas-struggle/Assets/Configs/Secrets.cs
+++ klas-struggle/Assets/Configs/Secrets.cs
@@ -8,8 +8,8 @@ namespace Assets.Scripts
 {
     static class Secrets
     {
-        public const string FirebaseAPIKey = "AIzaSyAxN5gNF3hWGp3PD4k0Dy9KI6KexcpULK0";
-        public const string FirebaseURL = "https://klas-struggle-81f14.firebaseio.com/";
+        public const string FirebaseAPIKey = "<>";
+        public const string FirebaseURL = "<>";
     }
 
 }
