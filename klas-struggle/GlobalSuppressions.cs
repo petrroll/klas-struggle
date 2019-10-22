86019c56369895d5131619376ff795521a0b4673
Petr Houška <houskape@gmail.com>
2019-10-23 01:39:17 +0200

    Add suppression for Roslyn diagnostics.

---
+++ klas-struggle/GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2235:Mark all non-serializable fields", Justification = "Unity serialization.", Scope = "member", Target = "~F:Assets.Scripts.KlasStruggle.Wheat.WheatState.Loc")]
