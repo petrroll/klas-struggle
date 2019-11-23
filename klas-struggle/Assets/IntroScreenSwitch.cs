6e98975dc51d5d0dc3b8252beb0b4b5b2925a370
Ondřej Trhoň <ondrej@trhon.cz>
2019-11-23 18:34:49 +0100

    Intro template

---
+++ klas-struggle/Assets/IntroScreenSwitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroScreenSwitch : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            Invoke("SwitchScene",0);
+        }
+
+    }
+
+    void SwitchScene() {
+        SceneManager.LoadScene("wheat-gen");
+    }
+}
