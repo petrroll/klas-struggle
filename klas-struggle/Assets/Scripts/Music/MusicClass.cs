b3b24ed82535416144a21377e038d70683df06a3
Petr Houška <houskape@gmail.com>
2020-04-05 00:28:40 +0200

    Cleanup.

---
+++ klas-struggle/Assets/Scripts/Music/MusicClass.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.KlasStruggle.Persistent
+{
+    public class MusicClass : MonoBehaviour
+    {
+        private AudioSource _audioSource;
+        private void Awake()
+        {
+            DontDestroyOnLoad(transform.gameObject);
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        public void PlayMusic()
+        {
+            if (_audioSource.isPlaying) return;
+            _audioSource.Play();
+        }
+
+        public void StopMusic()
+        {
+            _audioSource.Stop();
+        }
+    }
+}
---
+++ klas-struggle/Assets/Scripts/Music/StartMusic.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.KlasStruggle.Persistent
+{
+    public class StartMusic : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            GameObject.FindGameObjectWithTag("Music").GetComponent<MusicClass>().PlayMusic();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
