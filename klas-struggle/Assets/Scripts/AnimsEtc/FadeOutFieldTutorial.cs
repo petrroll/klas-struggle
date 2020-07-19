7792920732e02c378a2dd7c2465ba12d6e3016f1
Ondřej Trhoň <ondrej@trhon.cz>
2020-07-20 00:08:34 +0200

    Reworked FadeOut script, fixes to intro (removed canvas), improptu tutorial text added to field

---
+++ klas-struggle/Assets/Scripts/AnimsEtc/FadeOutFieldTutorial.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Assets.Scripts.AnimsEtc
+{
+    public class FadeOutFieldTutorial : MonoBehaviour
+    {
+        public float FadeOutTime = 3f;
+
+        bool alreadyFired = false;
+        void Update()
+        {
+
+            //We will need to chain this to unlocking players movement so you cant erase it during fadein.
+            if (Input.anyKeyDown || Input.GetMouseButtonDown(0) && !alreadyFired)
+            {
+                alreadyFired = true;
+                GetComponent<TextMeshPro>().DOFade(0f, FadeOutTime);
+            }
+        }
+    }
+}
